e95f7b17a8480c5d186d04d6fc659aa1d6168067
Ge <gmarcoscardoso@gmail.com>
2021-10-28 09:30:38 -0300

    feat: Add route home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      ola
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
