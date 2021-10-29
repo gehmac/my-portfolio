3782cfa8e3f8832d69a3ab55e2f32023f1f01c00
Ge <gmarcoscardoso@gmail.com>
2021-10-29 20:24:29 -0300

    feat: particules

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import HeaderPage from './components/Header';
+import Particle from './components/Particle';
 
 function App() {
   return (
     <Router>
+      <div className="particle">
+        <Particle />
+      </div>
       <HeaderPage />
       <Switch>
 
